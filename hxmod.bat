8783ed833a49b07099b056382d2a052d0d8d348c
Shorif Mia <sa21iu.cse@gmail.com>
2026-02-03 01:36:56 +0600

    Remove redundant documentation and test files

--- hxmod.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM HxMod v1.0 - Windows Batch Wrapper
-REM Execute HxMod using Python
-
-python "%~dp0hash_identifier.py" %*
--- test_analyzer.py
+++
@@ -1,9 +0,0 @@
-from lib.hash_analyzer import HashAnalyzer
-
-analyzer = HashAnalyzer()
-test_hash = '*81F5E21E35407D884A6CD4A731AEBFB6AF209E1B'
-
-result = analyzer.identify_hash(test_hash)
-print("Result:")
-for match in result['matches']:
-    print(match)
